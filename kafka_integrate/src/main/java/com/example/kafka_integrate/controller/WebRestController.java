f65d78954e73e59155f796574ac47a980b73da42
Muhammad Bilal <bilaltashfeen@gmail.com>
2017-11-26 10:21:22 -0800

    Add files via upload

---
+++ kafka_integrate/src/main/java/com/example/kafka_integrate/controller/WebRestController.java
@@ -0,0 +1,38 @@
+package com.example.kafka_integrate.controller;
+
+import com.example.kafka_integrate.services.KafkaProducer;
+import com.example.kafka_integrate.storage.MessageStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.javasampleapproach.apachekafka.services.KafkaProducer;
+//import com.javasampleapproach.apachekafka.storage.MessageStorage;
+
+@RestController
+@RequestMapping(value="/jsa/kafka")
+public class WebRestController {
+	
+	@Autowired
+	KafkaProducer producer;
+	
+	@Autowired
+	MessageStorage storage;
+	
+	@GetMapping(value="/producer")
+	public String producer(@RequestParam("data")String data){
+		producer.send(data);
+		
+		return "Done";
+	}
+	
+	@GetMapping(value="/consumer")
+	public String getAllRecievedMessage(){
+		String messages = storage.toString();
+		storage.clear();
+		
+		return messages;
+	}
+}
